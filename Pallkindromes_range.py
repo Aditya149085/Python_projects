74edb1ccbf0010b88b5b4876c6cd5cac7141e2bc
Aditya14_9085 <52645320+Aditya149085@users.noreply.github.com>
2022-09-13 20:13:47 +0530

    some errors but i will correct it

---
+++ Pallkindromes_range.py
@@ -0,0 +1,20 @@
+ans = "y"
+while ans != "n":
+    num = int(input("enter the number"))
+    for i in range(1,num+1):
+        if i<10:
+            print(i)
+        elif i<100 and i%11==0:
+            print(i)
+        elif i>100:
+            r = 0
+            number = i
+            while number > 0:
+                d = number%10
+                r = r*10+d
+                number = number//10
+            if number == i:
+                print(i)
+            else:
+                None
+    ans = input("press n to stop")
